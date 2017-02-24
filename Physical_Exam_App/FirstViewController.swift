ee40acf1464d3c75d8fde7a09ed9f79c0504afc2
Yuchen Qian <yqian14@illinois.edu>
2017-02-23 21:07:09 -0500

    Add a collection view and collection view cells.

--- Physical_Exam_App/FirstViewController.swift
+++ Physical_Exam_App/FirstViewController.swift
@@ -8,17 +8,43 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    let reuseIdentifier = "SectionCell"
+    var sections = ["A.Vital Signs","B.Head","C.Respiratory", "D.Cardiovascular","E: Abdomen","F.Lymph","G.Neurological","H.Breast","K.Musculoskeletal"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //self.view.addSubview(collectionView)
         // Do any additional setup after loading the view, typically from a nib.
+        //let nib = UINib(nibName: "SectionCollectionViewCell", bundle: nil)
+        //self.collectionView.register(nib, forCellWithReuseIdentifier: "SectionCell")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - UICollectionViewDataSource protocol
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print(sections.count)
+        return sections.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! SectionCollectionViewCell
+        //cell.myLabel.text = self.items[indexPath.item]
+        cell.myLabel.text = self.sections[indexPath.item]
+        //cell.backgroundColor = UIColor.cyan
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        print("You selected cell #\(indexPath.item)")
     }
 
 
---
+++ Physical_Exam_App/SectionCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  SectionCollectionViewCell.swift
+//  Physical_Exam_App
+//
+//  Created by Yuchen Qian on 2/23/17.
+//  Copyright © 2017 YuanHang. All rights reserved.
+//
+
+import UIKit
+
+class SectionCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var myLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
