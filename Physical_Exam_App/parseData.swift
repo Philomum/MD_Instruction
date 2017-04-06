d2544dfe59fc445f03c3afe0edf92f0998d34393
Yuchen Qian <rain@Yuchens-MacBook-Air.local>
2017-04-06 16:39:15 -0400

    add parse url

--- Physical_Exam_App/parseData.swift
+++ Physical_Exam_App/parseData.swift
@@ -8,46 +8,51 @@
 
 import Foundation
 
-let str1 = "C.Respiratory/Inspection/Posterior and Anterior lung fields with deep breaths (verbalize inspection)"
-let str2 = "C.Respiratory/Posterior : palpate, percuss, auscultate/Correct Technique: cross arms, hands on shoulder to raise scapulae"
-let str3 = "C.Respiratory/Posterior : palpate, percuss, auscultate/Check respiratory (thoracic) expansion"
+let str1 = "C.Respiratory/Inspection/Posterior and Anterior lung fields with deep breaths (verbalize inspection);https://www.youtube.com/watch?v=-EWLR4Xox_4&feature=youtu.be&app=desktop"
+let str2 = "C.Respiratory/Posterior : palpate, percuss, auscultate/Correct Technique: cross arms, hands on shoulder to raise scapulae;https://www.youtube.com/watch?v=-EWLR4Xox_4&feature=youtu.be&app=desktop"
+let str3 = "C.Respiratory/Posterior : palpate, percuss, auscultate/Check respiratory (thoracic) expansion;https://www.youtube.com/watch?v=-EWLR4Xox_4&feature=youtu.be&app=desktop"
 let list = [str1, str2, str3]
 
 var treeList = [treeNode]()
 
 
-func createTreeNode(array: [String], index: Int) -> treeNode{
+func createTreeNode(array: [String], index: Int, url: String) -> treeNode{
     let node = treeNode(name: array[index])
     if index + 1 == array.count {
         node.isInstruction = true
-        node.childInstruction = createInstructionNode(array: array, index: index)
+        node.childInstruction = createInstructionNode(array: array, index: index, url: url)
     }
     else {
-        node.children.append(createTreeNode(array: array, index: index + 1))
+        node.children.append(createTreeNode(array: array, index: index + 1, url: url))
     }
     
     return node
 }
 
-func createInstructionNode(array: [String], index: Int) -> Instruction {
+func createInstructionNode(array: [String], index: Int, url: String) -> Instruction {
     let ins = Instruction(name: array[index])
+    ins.source = url
     return ins
 }
 
-func parse() {
+func parse () {
     for str in list {
-        var arr = str.components(separatedBy: "/")
+        // choose your separate symbol
+        var array = str.components(separatedBy: ";")
+        var arr = array[0].components(separatedBy: "/")
+        let url = array[1]
+        //print(arr)
         var tempNode: treeNode
         tempNode = treeNode(name: arr[0])
         treeList.append(tempNode)
-        tempNode.children.append(createTreeNode(array: arr, index: 1))
+        tempNode.children.append(createTreeNode(array: arr, index: 1, url: url))
     }
     
     for treeNode in treeList {
         for child in treeNode.children {
             for children in child.children {
                 if children.isInstruction == true {
-                    print((children.childInstruction?.name)! as String)
+                    print((children.childInstruction?.source)! as String)
                 }
             }
         }
