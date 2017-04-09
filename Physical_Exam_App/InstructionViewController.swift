67cf078b3f6b02aa3bc0baab60f67d4ba043c146
Nevermore <xc98@duke.edu>
2017-04-09 15:30:51 -0400

    lol

--- Physical_Exam_App/InstructionViewController.swift
+++ Physical_Exam_App/InstructionViewController.swift
@@ -17,6 +17,7 @@ class InstructionViewController: UIViewController,UIWebViewDelegate,WKUIDelegate
     let modelName = UIDevice.current.modelName
     //hard coded url
     var urlString:String=""
+    var lol:String="lol"
     
     var webView : WKWebView!
     
